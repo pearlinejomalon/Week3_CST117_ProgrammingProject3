0c7b63744b8ae6b5fd80d2e1797da1b8e321abd0
pearlinejomalon <43321994+pearlinejomalon@users.noreply.github.com>
2019-01-28 00:18:09 -0600

    Add files via upload

---
+++ ProgrammingProject3/ProgrammingProject3/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ProgrammingProject3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(494, 234);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 54);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Clear Contents";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(644, 234);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 54);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(162, 234);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(144, 54);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Save File";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 299);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Programming Project 3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ ProgrammingProject3/ProgrammingProject3/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ProgrammingProject3
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog dialog = new OpenFileDialog();
+        SaveFileDialog saver = new SaveFileDialog();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string firstA = " ";
+        string lastA = " ";
+        string longestW = " ";
+        string totalV = " ";
+        char[] vowelArray;
+        int vowelCount;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            saver.Title = "Save Text File";
+            saver.Filter = "TXT | *.txt";
+            saver.InitialDirectory = @"C:\";
+            saver.ValidateNames = true;
+
+            {
+                if (saver.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamWriter swriter = new StreamWriter(saver.FileName))
+                    {
+                        await swriter.WriteLineAsync(textBox1.Text);
+                        MessageBox.Show("File Saved", " ", MessageBoxButtons.OK);
+                        swriter.Close();
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            dialog.Title = "Open Text File";
+            dialog.Filter = "TXT | *.txt";
+            dialog.InitialDirectory = @"C:\";
+            dialog.Multiselect = false;
+            dialog.ValidateNames = true;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                List<string> wordsTest = new List<string>();
+                using (StreamReader sreader = new StreamReader(dialog.FileName))
+                {
+
+                    while (!sreader.EndOfStream)
+                    {
+                        textBox1.Text = await sreader.ReadToEndAsync();
+
+                        string sread = sreader.ReadLine();
+                        string[] wordz = sread.Split();
+                        foreach (string abc in wordz)
+                        {
+                            if (abc.Length > 0)
+                            {
+                                string abcLower = abc.ToLower();
+                                if (firstA == "")
+                                    firstA = abcLower;
+                                if (abc.CompareTo(firstA) < 0)
+                                    firstA = abcLower;
+                                if (lastA == "")
+                                    lastA = abcLower;
+                                if (abc.CompareTo(lastA) < 0)
+                                    lastA = abcLower;
+                                if (longestW == "")
+                                    longestW = abcLower;
+                                if (abc.Length > longestW.Length)
+                                    longestW = abcLower;
+
+                                for (int j = 0; j < vowelArray.Length; j++)
+                                {
+                                    if (vowelArray[j] == 'a' ||
+                                        vowelArray[j] == 'e' ||
+                                        vowelArray[j] == 'i' ||
+                                        vowelArray[j] == 'o' ||
+                                        vowelArray[j] == 'u')
+
+                                    {
+                                        vowelCount++;
+
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                {
+
+                    textBox1.Text = "First Alphabet: " + firstA + "\nLast Alphabet: " + lastA + "\nLongetWord" + longestW + "\nVowel Count: " + vowelCount;
+                }
+
+            }
+        }
+    }
+}
